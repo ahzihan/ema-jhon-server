59a073507b14fb6a9713d9de0734bb27ea3bc446
Md Akbar Hossain <akbarhossain160@gmail.com>
2022-07-14 17:05:34 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require( 'express' );
+const { MongoClient, ServerApiVersion } = require( 'mongodb' );
+const cors = require( 'cors' );
+const app = express();
+const port = process.env.PORT || 5000;
+require( 'dotenv' ).config();
+
+//middleware
+
+app.use( cors() );
+app.use( express.json() );
+
+
+
+
+const uri = `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASS }@cluster0.b4jezta.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 } );
+
+
+
+
+app.get( '/', ( req, res ) => {
+    res.send( 'Jhon is Ready' );
+} );
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db( "emaJhon" ).collection( "product" );
+
+        app.get( '/product', async ( req, res ) => {
+            const page = parseInt( req.query.page );
+            const size = parseInt( req.query.size );
+            const query = {};
+            const cursor = productCollection.find( query );
+            let products;
+            if ( page || size ) {
+                products = await cursor.skip( page * size ).limit( size ).toArray();
+            } else {
+                products = await cursor.toArray();
+            }
+
+            res.send( products );
+        } );
+
+        app.get( '/productCount', async ( req, res ) => {
+            const count = await productCollection.estimatedDocumentCount();
+            res.send( { count } );
+        } );
+
+        //use post to get products by ids
+
+
+
+    }
+    finally {
+
+    }
+}
+run().catch( console.dir );
+
+
+
+app.listen( port, () => {
+    console.log( 'Ema and Jhon use poart: ', port );
+} );
